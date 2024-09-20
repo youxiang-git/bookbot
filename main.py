3ee023a7814f3aa5dc43dd254937460364288381
youxiang-git <chaiyouxiang@gmail.com>
2024-09-20 15:08:27 +0800

    add report generator

--- main.py
+++ main.py
@@ -4,9 +4,33 @@ def main():
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     char_count = get_char_count(text)
+    list_char_count = convert_to_dict_list(char_count)
+    list_char_count.sort(reverse=True, key=sort_on)
 
-    print(f"There are {num_words} words in the text")
-    print(char_count)
+    generate_report(num_words, list_char_count)
+
+
+def generate_report(num_words, list_char_count):
+    print("--- Begin report of books/frankenstein.txt ---")
+    print(f"{num_words} words found in the document")
+    print()
+    for count in list_char_count:
+        print(f"The '{count['char']}' character was found {count['num']} times")
+    print("--- End report ---")
+
+def convert_to_dict_list(dict):
+    list_of_dicts = []
+    for key, value in dict.items():
+        if key.isalpha():
+            list_of_dicts.append({"char": key, "num": value})
+
+    return list_of_dicts
+
+def sort_on(dict):
+    return dict["num"]
+
+def sort_on(dict):
+    return dict["num"]
 
 def get_char_count(text):
     char_dict = {}
